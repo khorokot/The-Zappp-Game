d33c7d1dfb59c7ba61a5e890981ad88150adaaa4
rossarin <rosa_sai@hotmail.com>
2012-10-26 10:29:02 +0700

    Test 5x5 win 4

---
+++ ZicZacZappp - Sp4-2/Test/BettleTest.cs
@@ -0,0 +1,101 @@
+﻿using ZicZacZappp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for BettleTest and is intended
+    ///to contain all BettleTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BettleTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for CheckForWin
+        ///</summary>
+        [TestMethod()]
+        //public void CheckForWinTest()
+        //{
+        //    string P1 = "AAA"; // TODO: Initialize to an appropriate value
+        //    string P2 = "BBB"; // TODO: Initialize to an appropriate value
+        //    int mode = 1; // TODO: Initialize to an appropriate value
+        //    Bettle target = new Bettle(P1, P2, mode); // TODO: Initialize to an appropriate value
+        //    string[] array = { "0", "x", "x", "0", "0", "0", "0", "0", "0" }; // TODO: Initialize to an appropriate value
+        //    bool expected = false; // TODO: Initialize to an appropriate value
+        //    bool actual;
+        //    actual = target.CheckForWin(array);
+        //    Assert.AreEqual(expected, actual);
+        //    //Assert.Inconclusive("Verify the correctness of this test method.");
+        //}
+
+        public void CheckForWinTest()
+        {
+            string P1 = "AAA"; // TODO: Initialize to an appropriate value
+            string P2 = "BBB"; // TODO: Initialize to an appropriate value
+            int mode = 1; // TODO: Initialize to an appropriate value
+            Bettle target = new Bettle(P1, P2, mode); // TODO: Initialize to an appropriate value
+            string[] array = { "x", "x", "x", "0", "0", "0", "0", "0", "0" }; // TODO: Initialize to an appropriate value
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.CheckForWin(array);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+
+    }
+}
---
+++ ZicZacZappp - Sp4-2/ZicZacZappp/Bettle.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace ZicZacZappp
+{
+    partial class Bettle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.PlayerXsc = new System.Windows.Forms.TextBox();
+            this.PlayerOsc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.ResetSC = new System.Windows.Forms.Button();
+            this.msgStatus = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bsh = new System.Windows.Forms.PictureBox();
+            this.wsh = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bsh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wsh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Clip", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(83, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zic Zac Zappp";
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(55, 53);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(90, 90);
+            this.bt1.TabIndex = 1;
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(151, 53);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(90, 90);
+            this.bt2.TabIndex = 2;
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(247, 53);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(90, 90);
+            this.bt3.TabIndex = 3;
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(55, 149);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(90, 90);
+            this.bt4.TabIndex = 4;
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(151, 149);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(90, 90);
+            this.bt5.TabIndex = 5;
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(247, 149);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(90, 90);
+            this.bt6.TabIndex = 6;
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.Location = new System.Drawing.Point(55, 245);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(90, 90);
+            this.bt7.TabIndex = 7;
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.Location = new System.Drawing.Point(151, 245);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(90, 90);
+            this.bt8.TabIndex = 8;
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.Location = new System.Drawing.Point(247, 245);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(90, 90);
+            this.bt9.TabIndex = 9;
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // PlayerXsc
+            // 
+            this.PlayerXsc.Location = new System.Drawing.Point(17, 383);
+            this.PlayerXsc.Name = "PlayerXsc";
+            this.PlayerXsc.ReadOnly = true;
+            this.PlayerXsc.Size = new System.Drawing.Size(35, 20);
+            this.PlayerXsc.TabIndex = 10;
+            // 
+            // PlayerOsc
+            // 
+            this.PlayerOsc.Location = new System.Drawing.Point(334, 383);
+            this.PlayerOsc.Name = "PlayerOsc";
+            this.PlayerOsc.ReadOnly = true;
+            this.PlayerOsc.Size = new System.Drawing.Size(35, 20);
+            this.PlayerOsc.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Player X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Player O";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Reset
+            // 
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Reset.Location = new System.Drawing.Point(151, 359);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(90, 23);
+            this.Reset.TabIndex = 14;
+            this.Reset.Text = "Restart";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // ResetSC
+            // 
+            this.ResetSC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ResetSC.Location = new System.Drawing.Point(135, 388);
+            this.ResetSC.Name = "ResetSC";
+            this.ResetSC.Size = new System.Drawing.Size(129, 25);
+            this.ResetSC.TabIndex = 15;
+            this.ResetSC.Text = "Reset scroll";
+            this.ResetSC.UseVisualStyleBackColor = true;
+            this.ResetSC.Click += new System.EventHandler(this.ResetSC_Click);
+            // 
+            // msgStatus
+            // 
+            this.msgStatus.AllowDrop = true;
+            this.msgStatus.Location = new System.Drawing.Point(135, 425);
+            this.msgStatus.Multiline = true;
+            this.msgStatus.Name = "msgStatus";
+            this.msgStatus.ReadOnly = true;
+            this.msgStatus.Size = new System.Drawing.Size(234, 20);
+            this.msgStatus.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nauvoo", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 430);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Message Status";
+            // 
+            // bsh
+            // 
+            this.bsh.Image = global::ZicZacZappp.Properties.Resources.bs3;
+            this.bsh.Location = new System.Drawing.Point(17, 342);
+            this.bsh.Name = "bsh";
+            this.bsh.Size = new System.Drawing.Size(35, 35);
+            this.bsh.TabIndex = 18;
+            this.bsh.TabStop = false;
+            // 
+            // wsh
+            // 
+            this.wsh.Image = global::ZicZacZappp.Properties.Resources.ws3;
+            this.wsh.Location = new System.Drawing.Point(334, 347);
+            this.wsh.Name = "wsh";
+            this.wsh.Size = new System.Drawing.Size(35, 35);
+            this.wsh.TabIndex = 19;
+            this.wsh.TabStop = false;
+            // 
+            // Bettle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = global::ZicZacZappp.Properties.Resources.sheep;
+            this.ClientSize = new System.Drawing.Size(384, 462);
+            this.Controls.Add(this.wsh);
+            this.Controls.Add(this.bsh);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.msgStatus);
+            this.Controls.Add(this.ResetSC);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PlayerOsc);
+            this.Controls.Add(this.PlayerXsc);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(400, 500);
+            this.MinimumSize = new System.Drawing.Size(400, 500);
+            this.Name = "Bettle";
+            this.Text = "Battle";
+            this.Load += new System.EventHandler(this.Bettle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bsh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wsh)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.TextBox PlayerXsc;
+        private System.Windows.Forms.TextBox PlayerOsc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button ResetSC;
+        private System.Windows.Forms.TextBox msgStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox bsh;
+        private System.Windows.Forms.PictureBox wsh;
+    }
+}
+
